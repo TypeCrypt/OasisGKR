f4e36f2fdd401b186ce7270ff4d8cecc86f3e668
Naman Kumar <namankr02@gmail.com>
2022-05-29 12:34:32 +0530

    addition function added to `FField`

--- src/multilinear-sumcheck/FField.java
+++ src/multilinear-sumcheck/FField.java
@@ -2,13 +2,27 @@ import java.math.BigInteger;
 
 public class FField extends Number{
 
+    /* FField is implemented as a number type. Alternately
+    it can store methods, but the rest is unclear.*/
+
     public BigInteger ff_prime;
     public BigInteger ff_elem;
+
+
+
+    // Constructors
 
     public FField(BigInteger ff_p, BigInteger ff_e) {
         this.ff_prime = ff_p;
         this.ff_elem = ff_e.mod(ff_p);
     }
+
+
+    /*
+    Methods required for extension to Number class.
+    All of them have been inherited from the BigInteger
+    class implementation.
+    */
 
     @Override
     public int intValue() {
@@ -29,4 +43,19 @@ public class FField extends Number{
     public double doubleValue() {
         return ff_elem.doubleValue();
     }
+
+
+
+    // Mathematical operators
+
+    public FField add(FField ff) throws Exception {
+        if (this.ff_prime == ff.ff_prime) {
+            return new FField(this.ff_prime, this.ff_elem.add(ff.ff_elem));
+        }
+        else{
+            throw new Exception("Primes do not match.");
+        }
+    }
+
+
 }
