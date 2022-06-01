bc1cd5d7047538071d7046f80f4c61fef937c01f
Naman Kumar <namankr02@gmail.com>
2022-06-01 23:01:54 +0530

    added a constructor for MLPoly, added comment for term representation

--- src/multilinear-sumcheck/MLPoly.java
+++ src/multilinear-sumcheck/MLPoly.java
@@ -5,12 +5,31 @@ import util.Tuple;
 
 public class MLPoly implements Poly {
     
+
+    // Public parameters: variable number and field
+
     public Integer var_no;
     public Field field;
+    
+
+    /* Term representation: Each term will have an associated
+     * binary string of length var_no. The ith bit acts as an 
+     * indicator as to whether the ith element is in that 
+     * particular term or not. */
+
     public Vector<Tuple<String, BigInteger>> terms;
+
+
+    // Constructors
 
     public MLPoly(Integer var_no, Field field, Vector<Tuple<String, BigInteger>> terms) {
         this.var_no = var_no;
+        this.field = field;
+        this.terms = terms;
+    }
+
+    public MLPoly(Field field, Vector<Tuple<String, BigInteger>> terms) {
+        this.var_no = terms.firstElement().x.length();
         this.field = field;
         this.terms = terms;
     }
