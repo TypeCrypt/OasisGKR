ab8198eaa58386fc48371b901f70c6cd475c6dd1
Naman Kumar <namankr02@gmail.com>
2022-06-02 08:49:34 +0530

    Poly implementation of univariate polynomial

--- src/multilinear-sumcheck/Poly.java
+++ src/multilinear-sumcheck/Poly.java
@@ -1,8 +1,5 @@
-import java.math.BigInteger;
-
 public interface Poly {
     public Integer degree();
     public Integer size();
     public Boolean is_constant();
-    public BigInteger evaluate(BigInteger a);
 }
--- src/multilinear-sumcheck/UPoly.java
+++ src/multilinear-sumcheck/UPoly.java
@@ -7,15 +7,45 @@ public class UPoly implements Poly {
 
 
     // Public parameters
+
     public Field field;
 
-
-    /* Term representation: Each term will have an associated
-     * binary string of length var_no. The ith bit acts as an 
-     * indicator as to whether the ith element is in that 
-     * particular term or not. */
+    
+    /* Term representation: ith bit of the n-bit vector represents
+     * the coefficient of x^n. */
 
     public Vector<BigInteger> terms;
 
+
+    // Constructors
+
+    public UPoly(Field field, Vector<BigInteger> terms) {
+        this.field = field;
+        this.terms = terms;
+    }
+
+
+    // Identity Checks
+
+    public Boolean is_constant() {
+        return (this.terms.size() == 1);
+    }
+
+
+    // Size Checks
+
+    public Integer degree() {
+        return this.terms.size()-1;
+    }
+
+    public Integer size() {
+        Integer counter = 0;
+        for (BigInteger elem : this.terms) {
+            if (elem != field.zero) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 
 }
